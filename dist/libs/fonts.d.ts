c81edef570799b22cd5e53f8e4ff25665ced48ed
qhnu <73204221+qhnu@users.noreply.github.com>
2021-02-22 21:23:16 +0900

    chore: build dist

--- dist/libs/fonts.d.ts
+++ dist/libs/fonts.d.ts
@@ -1,9 +1,9 @@
 import type { BinaryData, FontFaceDescriptors } from 'css-font-loading-module';
 export declare const downloadFontByStylesheet: (urls: string[]) => Promise<void>;
-export interface Font {
-    family: string;
+export interface Font<T extends string> {
+    family: T;
     source: string | BinaryData;
     descriptors?: FontFaceDescriptors;
 }
-export declare const downloadFontByBin: (fonts: Font[]) => Promise<void>;
+export declare const downloadFontByBin: <T extends string>(fonts: Font<T>[]) => Promise<void>;
 //# sourceMappingURL=fonts.d.ts.map
