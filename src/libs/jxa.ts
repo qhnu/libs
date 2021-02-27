dd5feaa61e7561daa560836c5dc85d144430885c
qhnu <73204221+qhnu@users.noreply.github.com>
2021-02-27 14:12:26 +0900

    feat: add jxa methods

---
+++ src/libs/jxa.ts
@@ -0,0 +1,124 @@
+/* eslint-disable */
+import * as Empty from '@jxa/global-type'
+import { run } from '@jxa/run'
+
+const CHROME_PATH = '/Applications/_Web/Google Chrome.app'
+const IRIS_PATH = '/Applications/_Video/Iris.app'
+
+export const fetchArgs = async (...args: any) => {
+  return await run((args) => {
+    const payload = args
+    return payload
+  }, args)
+}
+
+export const showUiElementName = async (
+  appPath: string,
+  filters: string[] = []
+) => {
+  return await run(
+    (appPath: string, filters: string[]) => {
+      const outputUiPath = (applicationName: string) => {
+        const process = Application('System Events').processes[applicationName]
+        for (const content of process.entireContents()) {
+          const displayString = Automation.getDisplayString(content)
+          if (filters.length) {
+            for (const row of displayString.split('\n')) {
+              const has = filters.some((filter) => row.includes(filter))
+              if (has) console.log('menu=', row)
+            }
+          } else {
+            console.log('menu=', displayString)
+          }
+        }
+      }
+
+      const app = Application(appPath)
+      if (!app.running()) app.launch()
+
+      outputUiPath(app.name())
+      console.log('name=', app.name())
+    },
+    appPath,
+    filters
+  )
+}
+
+export const resizeChrome = async () => {
+  return await run(() => {
+    const chrome = Application(CHROME_PATH)
+    chrome.activate()
+
+    let window = null
+    for (const i of Array(chrome.windows.length).keys()) {
+      if (!/^Dev/.test(chrome.windows[i].name())) {
+        window = chrome.windows[i]
+        break
+      }
+    }
+    window.bounds = { x: 0, y: 0, width: 1800, height: 1200 } // objでのsetが必要
+
+    let tab = null
+    for (const i of Array(window.tabs.length).keys()) {
+      if (/^http:\/\/localhost/.test(window.tabs[i].url())) {
+        tab = window.tabs[i]
+        window.activeTabIndex = i + 1 // インデックスは1から始まる
+        break
+      }
+    }
+  })
+}
+
+export const prepareIris = async () => {
+  return await run(() => {
+    const irisApp = Application(IRIS_PATH)
+    if (irisApp.running()) {
+      irisApp.quit()
+      delay(1)
+    }
+    irisApp.launch()
+    irisApp.activate()
+
+    const irisEvents = Application('System Events').processes[irisApp.name()]
+    irisEvents.menuBars
+      .at(0)
+      .menuBarItems.byName('File')
+      .menus.byName('File')
+      .menuItems.byName('New Recording...')
+      .click()
+  })
+}
+
+export const startRecord = async () => {
+  return await run(() => {
+    const irisApp = Application(IRIS_PATH)
+    const irisEvents = Application('System Events').processes[irisApp.name()]
+    irisEvents.windows
+      .byName('Settings Window')
+      .buttons.byName('Record')
+      .click()
+  })
+}
+
+export const stopRecord = async () => {
+  return await run(() => {
+    const irisApp = Application(IRIS_PATH)
+    const irisEvents = Application('System Events').processes[irisApp.name()]
+    irisEvents.menuBars
+      .at(0)
+      .menuBarItems.byName('File')
+      .menus.byName('File')
+      .menuItems.byName('Stop')
+      .click()
+
+    delay(1)
+
+    irisApp.quit()
+  })
+}
+
+export const sleepOs = async () => {
+  return await run(() => {
+    Application('System Events').sleep()
+  })
+}
